b7b149d3780ca9f7579ea1e1b9798d6e2c771c3c
hailm <hai.lemanh.1310@gmail.com>
2021-06-21 06:05:13 +0700

    update

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php $data["title"] = "Trang chủ"; ?>
 <?php $is_home = true; ?>
 <?php require "template-parts/layouts/header.php"; ?>
-
+<?php require "template-parts/layouts/booknow.php"; ?>
 <?php require "template-parts/layouts/qa.php"; ?>
 <?php require "template-parts/layouts/footer.php"; ?>
---
+++ template-parts/layouts/booknow.php
@@ -0,0 +1,32 @@
+<div class="uk-section booknow" uk-height-viewport>
+    <div class="uk-position-relative">
+        <div class="uk-container">
+            <div class="box1__margin">
+                <div class="uk-flex-center" uk-grid>
+                    <div class="uk-width-2-3@m">
+                        <div class="uk-text-center">
+                            <div class="uk-display-inline-block uk-text-left box1__box">
+                                <div class="box1__txt">BÁO GIÁ</div>
+                                <h2 class="uk-h2 uk-margin-remove box1__title">Gói giá & ưu đãi</h2>
+                            </div>
+                            <p class="box1__desc">Bạn có thể chọn lựa cho mình những gói giá trọn gói và khuyến mại có sẵn. Bạn cũng có thể tự tạo gói giá riêng của mình xin vui lòng liên hệ tới chúng tôi</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="uk-child-width-1-3@m uk-grid-match" uk-grid>
+                <div>
+                    <div class="uk-card uk-card-default uk-card-body">
+
+                    </div>
+                </div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
