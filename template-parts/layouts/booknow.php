4dd3540c9d118fe2ad6adcd2913ab77e00c21c07
hailm <hai.lemanh.1310@gmail.com>
2021-06-21 11:41:03 +0700

    update

--- template-parts/layouts/booknow.php
+++ template-parts/layouts/booknow.php
@@ -15,17 +15,68 @@
                 </div>
             </div>
             <div class="uk-child-width-1-3@m uk-grid-match" uk-grid>
+                <?php
+                $data = array(
+                    array(
+                        namePack => 'Relax Basic',
+                        price => '29',
+                        featuresList => array(
+                            'Lorem ipsum',
+                            'dolor sit',
+                            'adipiscing elit',
+                            'eiusmod tempor',
+                            'dolore magna',
+                            'minim veniam',
+                        ),
+                    ),
+                    array(
+                        namePack => 'Standard',
+                        price => '40',
+                        featuresList => array(
+                            'Unlimited chats and websites',
+                            'Customizable chat in 25 languages',
+                            'Desktop, mobile and browser agent apps',
+                            '24/7 customer support',
+                            '2 month chat history storage',
+                            'Basic CRM functions',
+                            'Team chats for an unlimited number of staff members',
+                        ),
+                    ),
+                    array(
+                        namePack => 'Exclusive',
+                        price => '60',
+                        featuresList => array(
+                            'All the benefits of Basic version, and:',
+                            'Unlimited chat history storage',
+                            'Proactively start chats and catch leads using Smart Triggers',
+                            'Complete visitor info including IP addresses, region, visit source and more',
+                            'Canned responses with an automatic helper',
+                            'Chat transfer between agents',
+                            'Monitor website visitors in real time',
+                            'Connect JivoChat with chatbots and don’t miss a single request',
+                            'Use built -in CRM to turn new customers into returning ones and grow your business',
+                        ),
+                    ),
+                );
+                foreach ($data as $k => $v): ?>
                 <div>
-                    <div class="uk-card uk-card-default uk-card-body">
+                    <div class="uk-card uk-card-default uk-card-body uk-flex uk-flex-column">
+                        <div>
 
+                        </div>
+                        <div class="uk-flex-auto">
+                            <ul class="uk-list booknow__featuresList">
+                                <?php foreach ($v['featuresList'] as $key => $value): ?>
+                                <li><span class="uk-position-top-left" uk-icon="icon: check; ratio: 0.8"></span><?= $value ?></li>
+                                <?php endforeach; ?>
+                            </ul>
+                        </div>
+                        <div>
+                            <a href="" class="uk-button uk-button-default booknow__btn">Đặt ngay</a>
+                        </div>
                     </div>
                 </div>
-                <div>
-
-                </div>
-                <div>
-
-                </div>
+                <?php endforeach; ?>
             </div>
         </div>
     </div>
