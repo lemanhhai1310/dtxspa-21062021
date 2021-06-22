2cdccf151e06b1c6da2999778f151b159f2539be
hailm <hai.lemanh.1310@gmail.com>
2021-06-23 00:43:38 +0700

    update

---
+++ template-parts/layouts/promotion.php
@@ -0,0 +1,20 @@
+<div class="uk-flex uk-flex-middle uk-background-norepeat uk-background-center-center promotion" data-src="images/bg-samp.jpeg" uk-img>
+    <div class="uk-section uk-width-1-1">
+        <div class="uk-container">
+            <div class="box1__margin">
+                <div class="uk-flex-left" uk-grid>
+                    <div class="uk-width-2-5@m">
+                        <div class="uk-text-left">
+                            <div class="uk-display-inline-block uk-text-left box1__box">
+                                <div class="box1__txt">KHUYẾN MẠI</div>
+                                <h2 class="uk-h2 uk-margin-remove box1__title">Khuyến mại HÈ</h2>
+                            </div>
+                            <p class="box1__desc">Sứ mệnh “ Wake up your beauty – đánh thức vẻ đẹp tiềm ẩn trong bạn” đưa các dịch vụ Spa công nghệ cao trở nên thân thiện hơn với tất cả mọi người</p>
+                            <p><a href="" class="uk-button uk-button-default block01__blog__btnLoadmore">XEM THÊM</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
