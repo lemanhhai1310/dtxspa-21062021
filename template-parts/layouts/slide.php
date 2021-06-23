0d91032c5f4d536d65f719b1b1b465962792d38b
hailm <hai.lemanh.1310@gmail.com>
2021-06-23 08:26:22 +0700

    update

---
+++ template-parts/layouts/slide.php
@@ -0,0 +1,15 @@
+<div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slideshow="ratio: false">
+
+    <ul class="uk-slideshow-items" uk-height-viewport="offset-top: true;">
+        <li>
+            <img src="images/slide-bg.jpeg" alt="" uk-cover>
+        </li>
+        <li>
+            <img src="images/slide-bg.jpeg" alt="" uk-cover>
+        </li>
+    </ul>
+
+    <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slideshow-item="previous"></a>
+    <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slideshow-item="next"></a>
+
+</div>
